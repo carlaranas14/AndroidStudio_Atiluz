88e212f79913f0c12321b403c71840af97998626
Carl Aranas <carlangeloaranas14@gmail.com>
2024-07-31 15:15:23 +0800

    customer dashboard created

--- app/src/main/java/com/example/atiluz/CustomerDasboard.java
+++ app/src/main/java/com/example/atiluz/CustomerDasboard.java
@@ -8,12 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class CustomerRegistration extends AppCompatActivity {
+public class CustomerDasboard extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_customer_registration);
+        setContentView(R.layout.activity_customer_dasboard);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
